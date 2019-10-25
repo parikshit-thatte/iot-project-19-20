3fd5a3da4160c4e4fff889c1eeff6a7bd95f9ef9
Parikshit Thatte <parikshitthatte23@gmail.com>
2019-10-25 16:33:54 +0530

    Almost done. 90% done. 25th Oct 2019 Dhanteras

--- addTrendings.php
+++ addTrendings.php
@@ -2,7 +2,7 @@
   <form action="<?php echo htmlspecialchars('addTrendingsToDB.php'); ?>" method="POST" class="form-group">
     <div class="row">
       <div class="col-25">
-          <label for="username">Title :</label>
+          <label for="title">Title :</label>
       </div>
       <div class="col-75">
           <input type="text" name="title"><br>
@@ -10,7 +10,7 @@
     </div>
     <div class="row">
       <div class="col-25">
-          <label for="username">Description :</label>
+          <label for="description">Description :</label>
       </div>
       <div class="col-75">
           <textarea name="description" rows="8" cols="80"></textarea>
